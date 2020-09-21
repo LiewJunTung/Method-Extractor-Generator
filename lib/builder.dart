a7de996242e75e61306749156165e7c51bb5e2d4
JT <liewjuntung@gmail.com>
2020-09-21 22:25:23 +0800

    first commit

---
+++ lib/builder.dart
@@ -0,0 +1,9 @@
+import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
+
+import 'src/generator.dart';
+
+Builder methodExtractorBuilder(BuilderOptions options) => LibraryBuilder(
+    MethodExtractorGenerator(),
+    formatOutput: (generated) => generated.replaceAll(RegExp(r'//.*|\s'), ''),
+    generatedExtension: '.gen.json');
---
+++ lib/src/generator.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/visitor.dart';
+import 'package:build/build.dart';
+import 'package:method_extractor/method_extractor.dart';
+import 'package:source_gen/source_gen.dart';
+
+class MethodExtractorGenerator extends GeneratorForAnnotation<MethodExtractor> {
+  @override
+  FutureOr<String> generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) {
+    if (element.metadata.isNotEmpty) {
+      var visitor = CheckNativeAnnotation();
+      element.visitChildren(visitor);
+      return jsonEncode({'name': 'GeneratedFlutterVerifytPlatformSDK', 'methods': visitor.methodList});
+    } else {
+      return null;
+    }
+  }
+}
+
+class CheckNativeAnnotation extends SimpleElementVisitor {
+  List<String> methodList = [];
+
+  @override
+  void visitMethodElement(MethodElement element) {
+    var ignoreAnnotation = element.metadata
+        .any((element) => element.element.name == 'ignoreNativeAnnotation');
+    if (!ignoreAnnotation) {
+      methodList.add(element.name);
+    }
+  }
+}
